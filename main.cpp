1daff17557faa71119f89f66755d7d37be5606de
xt1zer <xt1zer@outlook.com>
2019-03-01 23:22:27 +0300

    Testing latest updates

--- main.cpp
+++ main.cpp
@@ -7,21 +7,25 @@ int main() {
     Date today(2019, 2, 21);
 
     Employee t;
-    Employee trainee("Daniel", "White", "IT specialist", Date(1998, 12, 18), Date(2016, 12, 18), 15);
+    Employee trainee("Daniel", "White", "Programmer", "Head ", "IT Specialist", Date(1998, 12, 18), Date(2016, 12, 18), 15);
     Employee test(trainee);
-    Employee economist("Garry", "Hanson", "Economist", Date(1993, 5, 3), Date(2003, 9, 8), 12);
-    Employee scientist("Melody", "Brook", "Scientist", Date(1995, 10, 15), Date(2017, 4, 11), 18);
+    Employee economist("Garry", "Hanson", "Accountant", "", "Economics", Date(1993, 5, 3), Date(2003, 9, 8), 12);
+    Employee scientist("Melody", "Brook", "Scientist", "Junior ", "Physics", Date(1995, 10, 15), Date(2017, 4, 11), 18);
 
-    Employee arr[5]{trainee, test, economist};
+    Employee arr[4]{trainee, test, economist};
     arr[3] = scientist;
-    arr[4] = Employee();
 
-    arr[2].print_info();
+    arr[1].print_info();
     
 //    test.print_info();
     cout << arr[1].compute_experience(today) << endl;
     cout << scientist.compute_age(today) << endl;
     economist.set_salary_by_exp(today);
-    economist.print_info();
+    scientist.print_info(); cout << endl;
+
+    scientist.set_salary_by_rank();
+    scientist.print_info();
+
+//    sort_by_birth(arr, 4);
     return 0;
 }
