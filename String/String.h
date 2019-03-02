fbff074b880de27d85a94bedde7d3c6ca83d4e6e
Daniel <41012295+xt1zer@users.noreply.github.com>
2019-03-02 08:45:34 +0300

    Added comments

--- String/String.h
+++ String/String.h
@@ -5,11 +5,11 @@ class String {
 
 private:
 
-    size_t m_len;
+    size_t m_len; // length of string
     char * m_str;
 
-    void copy(const char *, const size_t &);
-    String* divide(const char *, const size_t &) const;
+    void copy(const char * str, const size_t & pos); // copies all chars from [pos]
+    String* divide(const char *, const size_t &) const; // for split() func
 
 public:
 
@@ -28,8 +28,8 @@ public:
     void print() const;
 
     void append(const String & str);
-    void append(const char * str);
-    String substr(const unsigned int & position, const unsigned int & num) const;
-    String* split(const char &) const;
-    void replace(const char *, const char *);
+    void append(const char * str); // to end of string
+    String substr(const unsigned int & pos, const unsigned int & num) const; // cuts [num] chars from [pos]
+    String * split(const char & delim) const; // places strings to array
+    void replace(const char * from, const char * to); // a substring to another one
 };
